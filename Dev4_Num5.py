bfcf4d06f55b8cbc703cf894dba23b5d0a6e7414
arianegouin <39052797+arianegouin@users.noreply.github.com>
2018-11-26 15:01:29 -0500

    code done

--- Dev4_Num5.py
+++ Dev4_Num5.py
@@ -8,8 +8,82 @@ class PB:
         self.Vo = 50    # MeV
         self.hw = 41 * self.A**(-1/3)
         self.D = 0.0225 * self.hw
-        self.C = 0.1*self.hw
+        self.C = 0.1 * self.hw
+
+        self.protons = [
+                     [1, 0, 0.5],
+                     [1, 1, 1.5],
+                     [1, 1, 0.5],
+                     [1, 2, 2.5],
+                     [2, 0, 0.5],
+                     [1, 2, 1.5],
+                     [1, 3, 3.5],
+                     [2, 1, 1.5],
+                     [1, 3, 2.5],
+                     [2, 1, 0.5],
+                     [1, 4, 4.5],
+                     [2, 2, 2.5],
+                     [1, 4, 3.5],
+                     [3, 0, 0.5],
+                     [2, 2, 1.5],
+                     [1, 5, 5.5],    # 82
+                     ]
+
+        self.neutrons = [
+                     [1, 0, 0.5],
+                     [1, 1, 1.5],
+                     [1, 1, 0.5],
+                     [1, 2, 2.5],
+                     [2, 0, 0.5],
+                     [1, 2, 1.5],
+                     [1, 3, 3.5],
+                     [2, 1, 1.5],
+                     [1, 3, 2.5],
+                     [2, 1, 0.5],
+                     [1, 4, 4.5],
+                     [2, 2, 2.5],
+                     [1, 4, 3.5],
+                     [3, 0, 0.5],
+                     [2, 2, 1.5],
+                     [1, 5, 5.5],    # 82
+
+                     [2, 3, 3.5],
+                     [1, 5, 4.5],
+                     [1, 6, 6.5],
+                     [3, 1, 1.5],
+                     [2, 3, 2.5],
+                     [3, 1, 0.5]    # 126
+                     ]
 
 
     def Enlsj(self,n,l,s,j):
-        return -self.Vo + self.hw * (2 * (n - 1) + l + 3/2) - self.D * l * (l + 1) - C * (j * (j + 1) - l * (l + 1) - s * (s + 1)) / 2
+        return - self.Vo + self.hw * (2 * (n - 1) + l + 3/2) - self.D * l * (l + 1) - self.C * (j * (j + 1) - l * (l + 1) - s * (s + 1)) / 2
+
+    def lastProton(self):
+        shell = self.protons[-1]
+        n, l, s, j = shell[0], shell[1], 0.5, shell[2]
+        return - self.Enlsj(n, l, s, j)
+
+    def lastNeutron(self):
+        shell = self.neutrons[-1]
+        n, l, s, j = shell[0], shell[1], 0.5, shell[2]
+        return - self.Enlsj(n, l, s, j)
+
+    def bindingEnergy(self):
+        protonsEnergy = 0
+        for shell in self.protons:
+            n, l, s, j = shell[0], shell[1], 0.5, shell[2]
+            protonsEnergy += self.Enlsj(n, l, s, j) * (2 * j + 1)
+        neutronsEnergy = 0
+        for shell in self.neutrons:
+            n, l, s, j = shell[0], shell[1], 0.5, shell[2]
+            neutronsEnergy += self.Enlsj(n, l, s, j)  * (2 * j + 1)
+        return - (protonsEnergy + neutronsEnergy)
+
+
+pb = PB()
+print('The last proton energy is', pb.lastProton(), 'MeV.')
+print('The last neutron energy is', pb.lastNeutron(), 'MeV.')
+print('The binding energy is', pb.bindingEnergy(), 'MeV.')
+
+
